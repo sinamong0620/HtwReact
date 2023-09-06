ef69d5f50965bb91227ab888ac9daeb2fef752fc
sinamong0620 <sinaskim60@gmail.com>
2023-09-07 00:09:59 +0900

    props 변경에 따른 component function evaluation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
 
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOuutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
---
+++ src/components/Demo/DemoOuutput.js
@@ -0,0 +1,5 @@
+import React from "react";
+const DemoOutput = (props) => {
+  return <>{props.show ? <p>This is new!</p> : ""}</>;
+};
+export default DemoOutput;
