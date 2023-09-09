c4d42e0dae3a0b7a6a8dd4e8e09bdc385258c101
sinamong0620 <sinaskim60@gmail.com>
2023-09-09 22:51:46 +0900

    React Hook practice

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import "./App.css";
 import DemoOutput from "./components/Demo/DemoOuutput";
 
 function App() {
-  //useCallback은 컴포넌트 실행 전반에 걸쳐 함수를 저장할 수 있게 하는 훅으로
-  //매번 실행때마다 이 함수를 재생성할 필요가 없다는 것을 알릴 수 있다.
-  //동일한 함수 객체가 메모리의 동일한 위치에 저장되므로 올바른 비교작업을 할 수 있다.
-  //리액트 내부 공간에 저장했다가 객체가 생성이 될때마다 이를 재사용할 수 있게 된다.
-  //저장하려는 함수를 래핑하면된다! usecallback도 useEffect처럼 의존성 배열이 존재한다.
+  // useCallback은 컴포넌트 실행 전반에 걸쳐 함수를 저장할 수 있게 하는 훅으로
+  // 매번 실행때마다 이 함수를 재생성할 필요가 없다는 것을 알릴 수 있다.
+  // 동일한 함수 객체가 메모리의 동일한 위치에 저장되므로 올바른 비교작업을 할 수 있다.
+  // 리액트 내부 공간에 저장했다가 객체가 생성이 될때마다 이를 재사용할 수 있게 된다.
+  // 저장하려는 함수를 래핑하면된다! usecallback도 useEffect처럼 의존성 배열이 존재한다.
   const [showParagraph, setShowParagraph] = useState(false);
   const [allowToggle, setAllowToggle] = useState(false);
 
